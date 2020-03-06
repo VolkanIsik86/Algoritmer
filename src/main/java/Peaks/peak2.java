bb137f9bbf3469126f997b87b8de50479cedc159
VolkanIsik <s180103@student.dtu.dk>
2020-03-06 19:04:47 +0100

    algoritmer

--- src/main/java/Peaks/peak2.java
+++ src/main/java/Peaks/peak2.java
@@ -1,4 +1,32 @@
 package Peaks;
 
+import java.util.Scanner;
+
 public class peak2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n ; i++) {
+            A[i] = scan.nextInt();
+        }
+        System.out.println(peak2(A,n));
+    }
+
+    public static int peak2(int[] A , int n){
+        int max = 0;
+        for (int i = 0; i < n ; i++) {
+            if (A[i] > A[max])
+                max = i;
+        }
+        return max;
+    }
 }
+
+
+
+//    FINDMAX(A, n)
+//max = 0
+//        for i = 0 to n-1
+//        if (A[i] > A[max]) max = i
+//        return max
--- src/main/java/Peaks/peak3.java
+++ src/main/java/Peaks/peak3.java
@@ -1,4 +1,42 @@
 package Peaks;
 
+import java.util.Scanner;
+
 public class peak3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n ; i++) {
+            A[i] = scan.nextInt();
+        }
+        System.out.println(peak3(A,0,n));
+    }
+    public static int peak3(int[] A , int i , int j){
+        int m = (i+j)/2;
+        if (m==0 && A[m]>A[m+1])
+            return m;
+        if (m==j-1 && A[m+1] > A[m])
+            return m;
+
+
+        if (A[m] >= A[m-1] && A[m] >= A[m+1]){
+            return m;
+        }
+        else if (A[m-1] > A[m]){
+            return peak3(A,i,m-1);
+        }
+        else if (A[m] < A[m+1]){
+            return peak3(A,m+1,j);
+        }
+        return -1;
+    }
 }
+
+//    PEAK3(A,i,j)
+//m = ⎣(i+j)/2)⎦
+//        if A[m] ≥ neighbors return m
+//        elseif A[m-1] > A[m]
+//        return PEAK3(A,i,m-1)
+//        elseif A[m] < A[m+1]
+//        return PEAK3(A,m+1,j)
